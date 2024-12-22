95dd175037a28eaf1fd2c1b8207c706ea8eded2c
kino6052 <113693383+nino-ux@users.noreply.github.com>
2024-12-22 19:55:45 +0300

    Chapter 10: Functions

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -8,28 +8,28 @@ import java.util.List;
 import java.util.Map;
 
 class Scanner {
-    private static final Map<String, TokenType> keywords;
+  private static final Map<String, TokenType> keywords;
 
   static {
     keywords = new HashMap<>();
-    keywords.put("and",    AND);
-    keywords.put("class",  CLASS);
-    keywords.put("else",   ELSE);
-    keywords.put("false",  FALSE);
-    keywords.put("for",    FOR);
-    keywords.put("fun",    FUN);
-    keywords.put("if",     IF);
-    keywords.put("nil",    NIL);
-    keywords.put("or",     OR);
-    keywords.put("print",  PRINT);
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
     keywords.put("return", RETURN);
-    keywords.put("super",  SUPER);
-    keywords.put("this",   THIS);
-    keywords.put("true",   TRUE);
-    keywords.put("var",    VAR);
-    keywords.put("while",  WHILE);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
   }
-  
+
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
   private int start = 0;
@@ -130,11 +130,13 @@ class Scanner {
   }
 
   private void identifier() {
-    while (isAlphaNumeric(peek())) advance();
+    while (isAlphaNumeric(peek()))
+      advance();
 
     String text = source.substring(start, current);
     TokenType type = keywords.get(text);
-    if (type == null) type = IDENTIFIER;
+    if (type == null)
+      type = IDENTIFIER;
     addToken(type);
   }
 
@@ -199,8 +201,8 @@ class Scanner {
 
   private boolean isAlpha(char c) {
     return (c >= 'a' && c <= 'z') ||
-           (c >= 'A' && c <= 'Z') ||
-            c == '_';
+        (c >= 'A' && c <= 'Z') ||
+        c == '_';
   }
 
   private boolean isAlphaNumeric(char c) {
